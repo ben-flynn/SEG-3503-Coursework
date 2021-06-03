8deeff7fdd6dfe8eabccb9cbed6cbbcd9eba906a
ben-flynn <55192923+ben-flynn@users.noreply.github.com>
2021-06-03 13:59:11 -0400

    Update Date.java

--- date/date/test/Date.java
+++ date/date/test/Date.java
@@ -43,10 +43,10 @@ public class Date {
 	 *   - less or equal to 30 for February if year is non-leap
 	 */
 	private void setDay(int day) {
-		if (day < 1) {
+		if (day <= 1) {
 			throw new IllegalArgumentException("day must greater or equal to 1.");
 		}
-		if (day > 31){
+		if (day >= 31){
 			throw new IllegalArgumentException("day must less or equal to 31.");
 		}
 		if (isThirtyDayMonth() && day > 30) {
